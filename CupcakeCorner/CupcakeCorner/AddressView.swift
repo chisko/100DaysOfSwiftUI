9a339d651a0e98505534f1f42f3b10a239525130
Francisco Ruiz <fcoruiz23@gmail.com>
2021-12-19 13:55:06 -0600

    Days 48 - 52; Cupcake Corner

---
+++ CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -0,0 +1,40 @@
+//
+//  AddressView.swift
+//  CupcakeCorner
+//
+//  Created by Francisco Ruiz on 19/12/21.
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    @ObservedObject var order: SharedOrder
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink {
+                    CheckoutView(order: order)
+                } label: {
+                    Text("Check out")
+                }
+            }
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AddressView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddressView(order: SharedOrder())
+    }
+}
---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Francisco Ruiz on 18/12/21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
